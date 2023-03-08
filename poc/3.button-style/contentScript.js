f397bbf07194dd0410e8f942439d2f1b69f9b4d3
배창현 <baechanghyeon@baechanghyeon-ui-MacBookAir.local>
2023-03-08 15:11:24 +0900

    feat: 정적페이지에서 table copy button injection

---
+++ poc/3.button-style/contentScript.js
@@ -0,0 +1,75 @@
+const btnShowList = document.createElement("button");
+btnShowList.setAttribute("id", "btn-show-list");
+btnShowList.innerText = "Show Button";
+
+const listContainer = document.createElement("ul");
+listContainer.setAttribute("id", "list_container");
+document.body.appendChild(btnShowList);
+document.body.appendChild(listContainer);
+
+const items = ["Download PDF", "Download PNG", "Export md", "Share Link"];
+
+const list = document.createElement("ul");
+
+for (let i = 0; i < items.length; i++) {
+  const item = document.createElement("li");
+  const item_button = document.createElement("button");
+  item_button.innerText = items[i];
+  item.appendChild(item_button);
+  list.appendChild(item);
+}
+
+listContainer.appendChild(list);
+
+btnShowList.addEventListener("click", () => {
+  if (list.style.display === "none") {
+    list.style.display = "block";
+  } else {
+    list.style.display = "none";
+  }
+});
+
+const listItems = document.querySelectorAll("#list_container li");
+
+listItems.forEach((item) => {
+  item.addEventListener("click", () => {
+    switch (item.innerText) {
+      case "Download PDF":
+        console.log("PDF");
+        break;
+      case "Download PNG":
+        console.log("PNG");
+        break;
+      case "Export md":
+        console.log("Export md");
+        break;
+      case "Share Link":
+        console.log("Share Link");
+        break;
+    }
+  });
+});
+
+// DOM example
+
+const init = function () {
+  const injectionElement = document.createElement("div");
+  injectionElement.setAttribute("id", "injection_element");
+  injectionElement.innerText = "Injection Element";
+  document.body.appendChild(injectionElement);
+
+  const hostElement = document.createElement("div");
+  hostElement.setAttribute("id", "injection_host");
+  hostElement.innerText = "Injection Host";
+  document.body.appendChild(hostElement);
+
+  // Using Shadow Root
+  var host = document.querySelector("#injection_host");
+  var root = host.attachShadow({ mode: open }); // Create Shadow Root
+  var div = document.createElement("div");
+  div.setAttribute("id", "div_root");
+  div.innerHTML = ` Hello From the Shadow Root Element`;
+  root.appendChild(div);
+};
+
+init();
