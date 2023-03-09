c7c880766e054c8149acdcb94314bafc7502f2dc
배창현 <baechanghyeon@baechanghyeon-ui-MacBookAir.local>
2023-03-09 23:46:19 +0900

    design: copy button CSS 수정

--- src/chrome-extension-boilerplate-react/src/pages/Content/index.js
+++ src/chrome-extension-boilerplate-react/src/pages/Content/index.js
@@ -8,7 +8,6 @@ const saveTheMdToClipboard = async (text) => {
 
 const convertHtmlToMd = (table, sort = 'left') => {
   const tr = table.getElementsByTagName('tr');
-
   // Table header
   let header = '|';
   const th = tr[0].getElementsByTagName('th');
@@ -48,27 +47,30 @@ const init = () => {
   table.forEach((table, idx) => {
     if (document.getElementById(`table_${idx}`)) return;
     table.setAttribute('id', `table_${idx}`);
-
+    table.setAttribute('class', 'm-0');
     // 각 테이블 마다 버튼 생성
-    const CopyButton = document.createElement('button');
-    CopyButton.setAttribute('id', `table_copy_button_${idx}`);
-    CopyButton.setAttribute('class', 'border-2 border-black ml-[428px]');
-    CopyButton.innerText = 'copy';
+    const copyButton = document.createElement('button');
+    copyButton.setAttribute('id', `table_copy_button_${idx}`);
+    copyButton.setAttribute(
+      'class',
+      'border border-black text-xs p-0.5 px-1 mb-1 rounded font-semibold hover:bg-gray-100 hover:text-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-gray-200'
+    );
+    copyButton.innerText = 'copy table';
 
     // div 요소 생성 및 속성 설정
     const div = document.createElement('div');
     div.classList.add('table-wrapper');
-    div.setAttribute('class', 'flex-row w-full');
+    div.setAttribute('class', 'w-full flex flex-col relative items-end mt-5');
 
     // div 부모 요소에 버튼 추가
-    div.appendChild(CopyButton);
+    div.appendChild(copyButton);
 
     // table 요소를 div 요소로 감싸기
     table.parentNode.insertBefore(div, table);
     div.appendChild(table);
 
     // Copy Button Event
-    CopyButton.addEventListener('click', () => {
+    copyButton.addEventListener('click', () => {
       convertHtmlToMd(table);
     });
   });
