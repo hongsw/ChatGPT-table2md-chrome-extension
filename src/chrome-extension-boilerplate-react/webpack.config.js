cc1b34411d0569bc48637fe8591c325ccd779892
Hong Buzz <hongbuzz@gmail.com>
2023-03-12 23:04:33 +0900

    🐛 remove dead code

--- src/chrome-extension-boilerplate-react/webpack.config.js
+++ src/chrome-extension-boilerplate-react/webpack.config.js
@@ -179,73 +179,14 @@ var options = {
         },
       ],
     }),
-    new CopyWebpackPlugin({
-      patterns: [
-        {
-          from: 'src/gtm.js',
-          to: path.join(__dirname, 'build'),
-          force: true,
-        },
-      ],
-    }),
-    // new CopyWebpackPlugin({
-    //   patterns: [
-    //     {
-    //       from: 'pages/Popup/popup.html',
-    //       to: path.join(__dirname, 'build'),
-    //       context: 'src',
-    //     },
-    //     {
-    //       from: 'pages/Popup/popup.js',
-    //       to: path.join(__dirname, 'build'),
-    //       context: 'src',
-    //     },
-    //     {
-    //       from: 'pages/Popup/popup.css',
-    //       to: path.join(__dirname, 'build'),
-    //       context: 'src',
-    //     },
-    //   ],
-    // }),
-    new CopyWebpackPlugin({
-      patterns: [
-        {
-          from: 'src/assets/img/icon.png',
-          to: path.join(__dirname, 'build'),
-          force: true,
-        },
-      ],
-    }),
-    // new HtmlWebpackPlugin({
-    //   template: path.join(__dirname, 'src', 'pages', 'Newtab', 'index.html'),
-    //   filename: 'newtab.html',
-    //   chunks: ['newtab'],
-    //   cache: false,
-    // }),
-    // new HtmlWebpackPlugin({
-    //   template: path.join(__dirname, 'src', 'pages', 'Options', 'index.html'),
-    //   filename: 'options.html',
-    //   chunks: ['options'],
-    //   cache: false,
-    // }),
+   
     new HtmlWebpackPlugin({
       template: path.join(__dirname, 'src', 'pages', 'Popup', 'index.html'),
       filename: 'popup.html',
       chunks: ['popup'],
       cache: false,
     }),
-    // new HtmlWebpackPlugin({
-    //   template: path.join(__dirname, 'src', 'pages', 'Devtools', 'index.html'),
-    //   filename: 'devtools.html',
-    //   chunks: ['devtools'],
-    //   cache: false,
-    // }),
-    // new HtmlWebpackPlugin({
-    //   template: path.join(__dirname, 'src', 'pages', 'Panel', 'index.html'),
-    //   filename: 'panel.html',
-    //   chunks: ['panel'],
-    //   cache: false,
-    // }),
+    
   ].filter(Boolean),
   infrastructureLogging: {
     level: 'info',
